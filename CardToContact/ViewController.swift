9f1d312dc6774d3f9f050997cb9d4edaee57a833
Griffin Solimni <solimini.1@osu.edu>
2017-08-08 21:15:00 -0400

    Initial Commit

---
+++ CardToContact/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardToContact
+//
+//  Created by Griffin Solimini on 8/8/17.
+//  Copyright © 2017 Griffin Solimini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
