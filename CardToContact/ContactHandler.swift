e4ac02ae213f91a3a0b44a450fc84760c25eb688
Griffin Solimni <solimini.1@osu.edu>
2017-08-10 23:02:27 -0400

    contact framework

--- CardToContact/ContactHandler.swift
+++ CardToContact/ContactHandler.swift
@@ -19,8 +19,23 @@ class ContactHandler {
         print(rawText)
         
         let contact = CNMutableContact()
-        let homePhone = CNLabeledValue(label: CNLabelHome, value: CNPhoneNumber(stringValue :"1234567890" ))
-        contact.phoneNumbers = [homePhone]
+        
+        contact.givenName = "First"
+        contact.familyName = "Last"
+        contact.organizationName = "Company"
+        contact.jobTitle = "Title"
+        
+        let homePhone = CNLabeledValue(label: CNLabelHome, value: CNPhoneNumber(stringValue :"2312312341"))
+        let cellPhone = CNLabeledValue(label: CNLabelPhoneNumberMobile, value: CNPhoneNumber(stringValue: "2312312341"))
+        let workPhone = CNLabeledValue(label: CNLabelWork, value: CNPhoneNumber(stringValue: "2312312341"))
+        let homeFax = CNLabeledValue(label: CNLabelPhoneNumberHomeFax, value: CNPhoneNumber(stringValue: "2345612341"))
+        let workFax = CNLabeledValue(label: CNLabelPhoneNumberWorkFax, value: CNPhoneNumber(stringValue: "2345612341"))
+        
+        let homeEmail = CNLabeledValue(label: CNLabelHome, value: NSString(string: "email@gmail.com"))
+        let workEmail = CNLabeledValue(label: CNLabelWork, value: NSString(string: "email@gmail.com"))
+        
+        contact.phoneNumbers = [homePhone, cellPhone, workPhone, homeFax, workFax]
+        contact.emailAddresses = [homeEmail, workEmail]
         
         return contact
         
